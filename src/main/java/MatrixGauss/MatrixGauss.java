5583021dbd1a1617aba9583975882bbde72bdc0e
Alexandr Litvin <klesh308@gmail.com>
2022-04-21 21:48:15 +0300

    fixed spaces

--- src/main/java/MatrixGauss/MatrixGauss.java
+++ src/main/java/MatrixGauss/MatrixGauss.java
@@ -6,7 +6,7 @@ import static MatrixGauss.MPOperations.*;
 import static MatrixGauss.Multiplication.multiply;
 
 public class MatrixGauss {
-   static final double[][] E = {
+    static final double[][] E = {
             {1, 0, 0, 0},
             {0, 1, 0, 0},
             {0, 0, 1, 0},
@@ -34,112 +34,112 @@ public class MatrixGauss {
     static final List<double[][]> PList = new ArrayList<>();
 
     /**
-     * Основна функція, що запускає увесь алгоритм методу
-     */
+    * Основна функція, що запускає увесь алгоритм методу
+    */
     public static void runMethod() {
-        for (int i = 0; i < LENGTH; i++) {
-            clearMP();                                  // Записуємо в Р та М одиничну матрицю
-            int ind = findLeadElem(i);                  // Знаходимо індекс ведучого елменту
+    for (int i = 0; i < LENGTH; i++) {
+        clearMP();                                  // Записуємо в Р та М одиничну матрицю
+        int ind = findLeadElem(i);                  // Знаходимо індекс ведучого елменту
 
-            if (ind != i) {                             // Якщо макс. елемент стовпчика не діагональний ->
-                calcP(i, ind);                          // Обчислюємо матрицю перестановки
-                prmtCount++;                            // +1 до кількості необхідних перестановок
-            }
-            calcM(i);                                   // Обчислюємо матрицю маштабування
-
-            addMPToList();                              // Додаємо матриці М та Р до списків
-            calcNewMtr();                               // Обчислємо нову матрицю
-            calcNewB();                                 // Обчислюємо новий стовпчик в
+        if (ind != i) {                             // Якщо макс. елемент стовпчика не діагональний ->
+            calcP(i, ind);                          // Обчислюємо матрицю перестановки
+            prmtCount++;                            // +1 до кількості необхідних перестановок
         }
-        calcX();                                        // Обчислюємо розв'язки матриці
-        calcDet();                                      // Обчислюємо визначник
-        calcInvMtr();                                   // Обчислюємо обернену матрицю
-        Printer.showResult(x, det, InvMtr);             // Вивід результатів
+        calcM(i);                                   // Обчислюємо матрицю маштабування
+
+        addMPToList();                              // Додаємо матриці М та Р до списків
+        calcNewMtr();                               // Обчислємо нову матрицю
+        calcNewB();                                 // Обчислюємо новий стовпчик в
+    }
+    calcX();                                        // Обчислюємо розв'язки матриці
+    calcDet();                                      // Обчислюємо визначник
+    calcInvMtr();                                   // Обчислюємо обернену матрицю
+    Printer.showResult(x, det, InvMtr);             // Вивід результатів
     }
 
     /**
-     * Знаходимо розв'язок матриці
-     */
+    * Знаходимо розв'язок матриці
+    */
     private static void calcX() {
-        x = backStroke(mtr, b);
+    x = backStroke(mtr, b);
     }
 
     /**
-     * Знаходимо визначник за формулою
-     */
+    * Знаходимо визначник за формулою
+    */
     private static void calcDet() {
-        det = Math.pow(-1, prmtCount);
-        for (double v : leadElem) {
-            det *= v;
-        }
+    det = Math.pow(-1, prmtCount);
+    for (double v : leadElem) {
+        det *= v;
+    }
     }
 
     /**
-     * Знаходимо обернену матрицю
-     */
+    * Знаходимо обернену матрицю
+    */
     private static void calcInvMtr() {
-        for (int i = 0; i < PList.size(); i++) {        // Знаходимо матрицю Ei
-            Ei = multiply(PList.get(i), Ei);
-            Ei = multiply(MList.get(i), Ei);
-        }
+    for (int i = 0; i < PList.size(); i++) {        // Знаходимо матрицю Ei
+        Ei = multiply(PList.get(i), Ei);
+        Ei = multiply(MList.get(i), Ei);
+    }
 
-        InvMtr = new double[LENGTH][LENGTH];
-        for (int i = 0; i < LENGTH; i++) {
-            double[] b1 = new double[LENGTH];
-            for (int j = 0; j < LENGTH; j++) {          // Виписуємо стовпчик з матриці Ei
-                b1[j] = Ei[j][i];
-            }
-            b1 = backStroke(mtr, b1);                   // Знаходимо стовбчик матриці InvMtr
-            for (int j = 0; j < LENGTH; j++) {
-                InvMtr[j][i] = b1[j];                   // Записуємо цей стовбчик в матрицю InvMtr
-            }
+    InvMtr = new double[LENGTH][LENGTH];
+    for (int i = 0; i < LENGTH; i++) {
+        double[] b1 = new double[LENGTH];
+        for (int j = 0; j < LENGTH; j++) {          // Виписуємо стовпчик з матриці Ei
+            b1[j] = Ei[j][i];
         }
+        b1 = backStroke(mtr, b1);                   // Знаходимо стовбчик матриці InvMtr
+        for (int j = 0; j < LENGTH; j++) {
+            InvMtr[j][i] = b1[j];                   // Записуємо цей стовбчик в матрицю InvMtr
+        }
+    }
     }
 
     /**
-     * Знаходимо наступну матрицю
-     */
+    * Знаходимо наступну матрицю
+    */
     private static void calcNewMtr() {
-        mtr = multiply(P, mtr);                         // Множимо основну матрицю на матрицю перестановки
-        mtr = multiply(M, mtr);                         // Множимо основну матрцию на матрицю маштабування
+    mtr = multiply(P, mtr);                         // Множимо основну матрицю на матрицю перестановки
+    mtr = multiply(M, mtr);                         // Множимо основну матрцию на матрицю маштабування
     }
 
     /**
-     * Знаходимо наступний стовбчик в
-     */
+    * Знаходимо наступний стовбчик в
+    */
     private static void calcNewB() {
-        b = multiply(P, b);                             // Множимо стовбчик в на матрицю перестановки
-        b = multiply(M, b);                             // Множимо стовбчик в на матрицю маштабування
+    b = multiply(P, b);                             // Множимо стовбчик в на матрицю перестановки
+    b = multiply(M, b);                             // Множимо стовбчик в на матрицю маштабування
     }
 
     /**
-     * Знаходимо ведучий елемент
-     */
+    * Знаходимо ведучий елемент
+    */
     private static int findLeadElem(int i) {
-        int ind = i;                                    // Діагональний елемент -> поч. макс. значення
-        double max = mtr[i][i];                         // Індекс цього елемнту
-        for (int j = i; j < LENGTH; j++) {              // Шукаємо в стовпчику максимальне значення
-            if (max < mtr[j][i]) {                      // Знайшли більше ніж початкове ->
-                max = mtr[j][i];                        // перезаписуємо його
-                ind = j;                                // Перезаписуємо його індекс
-            }
+    int ind = i;                                    // Діагональний елемент -> поч. макс. значення
+    double max = mtr[i][i];                         // Індекс цього елемнту
+    for (int j = i; j < LENGTH; j++) {              // Шукаємо в стовпчику максимальне значення
+        if (max < mtr[j][i]) {                      // Знайшли більше ніж початкове ->
+            max = mtr[j][i];                        // перезаписуємо його
+            ind = j;                                // Перезаписуємо його індекс
         }
-        leadElem[i] = max;                              // Додаємо до списку ведучих елементів
-        return ind;                                     // Повертаємо його індекс
+    }
+    leadElem[i] = max;                              // Додаємо до списку ведучих елементів
+    return ind;                                     // Повертаємо його індекс
     }
 
     /**
-     * Запускаэмо зворотній хід для
-     * знаходження розв'язків матриці
-     */
+    * Запускаэмо зворотній хід для
+    * знаходження розв'язків матриці
+    */
     private static double[] backStroke(double[][] m, double[] b1) {
-        double[] res = new double[b1.length];
-        for (int i = m.length - 1; i >= 0; i--) {
-            for (int j = i + 1; j < m.length; j++) {
-                b1[i] = b1[i] - m[i][j] * res[j];
-            }
-            res[i] = b1[i];
+    double[] res = new double[b1.length];
+    for (int i = m.length - 1; i >= 0; i--) {
+        for (int j = i + 1; j < m.length; j++) {
+            b1[i] = b1[i] - m[i][j] * res[j];
         }
-        return res;
+        res[i] = b1[i];
+    }
+    return res;
     }
 }
